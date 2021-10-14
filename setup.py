4cf1ad9cd73bd1d245d7a418dce7dc96a9c912c9
samhine <sam.hine27@gmail.com>
2021-10-14 18:10:45 +0100

    setup rename

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='lolpros_parser',
+setup(name='lolpros-parser',
       version='0.1',
       description='Python Parser for lolpros.gg',
       url='http://github.com/samhine/lolpros-parser',
