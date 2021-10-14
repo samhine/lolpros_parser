f176f1ef75cd49582c406a587063080a0360a869
samhine <sam.hine27@gmail.com>
2021-10-14 18:21:45 +0100

    setup fixes

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
-setup(name='lolpros-parser',
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='lolpros_parser',
       version='0.1',
       description='Python Parser for lolpros.gg',
       url='http://github.com/samhine/lolpros-parser',
+      download_url="https://github.com/samhine/lolpros_parser/archive/refs/tags/0.0.1.zip",
       author='Samuel Hine',
       author_email='sam.hine27@gmail.com',
+      keywords=['PYTHON', 'LOLPROS', 'API'],
       license='MIT',
-      packages=['requests'],
-      zip_safe=False)
+      install_requires=['requests'],
+      long_description_content_type='text/markdown',
+      long_description=long_description)
